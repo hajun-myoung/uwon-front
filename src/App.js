d2e5739d55ea632f9100c20d4d475bc7bf8828b3
hajun <audgkwns44@gmail.com>
2023-05-14 03:38:05 +0900

    [Refactoring]delete useless codes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Drawer_left from './Components/Drawer_left';
 import HtmlPage from './Pages/HtmlPage';
 import MainPage from './Pages/MainPage';
 import MuiPage from './Pages/MuiPage';
-import SparklistPage from './Pages/SparkDetail';
 
 const { kakao } = window;
 
@@ -22,7 +21,6 @@ function App() {
         <Route path="/mui" exact element={<MuiPage />} />
         <Route path="/html" exact element={<HtmlPage />} />
         <Route path="/nesting" exact element={<MuiPage />} />
-        <Route path="/SparklistPage" exact element={<SparklistPage />}/>
       </Routes>
     </Router>
     
