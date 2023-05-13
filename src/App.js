401f1d82b72bc0ce201ae3a7a5e7e6d825a29d9b
juminsung <juminsung10@gmail.com>
2023-05-13 20:35:09 +0900

    [Update]Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect} from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Drawer_left from './Components/Drawer';
+import HtmlPage from './Pages/HtmlPage';
+import MainPage from './Pages/MainPage';
+import MuiPage from './Pages/MuiPage';
+import NestingPage from './Pages/NestingPage';
+
+const { kakao } = window;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+  return (  
+    <Router>
+      <Routes>
+        <Route path="/drawer" exact element={<Drawer_left />} />
+        <Route path="/" exact element={<MainPage />} />
+        <Route path="/mui" exact element={<MuiPage />} />
+        <Route path="/html" exact element={<HtmlPage />} />
+        <Route path="/nesting" exact element={<NestingPage />} />
+      </Routes>
+    </Router>
+    
   );
 }
 
